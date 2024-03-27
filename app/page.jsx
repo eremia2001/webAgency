f8bb752bc324797bc26ce8e04b81b6dd060722c6
Eremiya Rifat <eremia2021@gmail.com>
2024-03-27 22:44:26 +0100

    placment hero section

--- app/page.jsx
+++ app/page.jsx
@@ -5,8 +5,8 @@ import { RxHamburgerMenu } from "react-icons/rx";
 export default function Home() {
   return (
     <main className="flex flex-col    ">
-      <div className="flex flex-col min-h-screen gradientBg">
-        <header className="flex items-center justify-between p-5 border-b-[1px]  border-[#E8F3F5] ">
+      <div className="flex flex-col  gradientBg ">
+        <header className=" flex items-center justify-between p-5 border-b-[1px]  border-[#E8F3F5] ">
           <div className="flex justify-start flex-1">
             <h1 className="font-bold text-white text-2xl">RifatIT.</h1>
           </div>
@@ -30,44 +30,46 @@ export default function Home() {
           </div>
         </header>
 
-        <div className="flex-1 px-4 sm:px-8  flex flex-col justify-center items-center text-center gap-4   font-mulish -translate-y-32 xl:-translate-y-14 ">
-          <h4 className="font-normal text-base xl:text-xl text-secondary">
-            Webdesign Agentur{" "}
-          </h4>
-          <div className="flex flex-col gap-1">
-            <h1 className="text-5xl sm:text-6xl lg:text-7xl  font-extrabold text-white text-center ">
-              Verkaufstarke Websites,{" "}
-            </h1>
-            <h2 className="text-2xl sm:text-4xl lg:text-5xl font-extrabold text-white text-center ">
-              die ihre Kunden überzeugen
-            </h2>
+        <div className=" flex flex-col justify-center items-center h-screen">
+          <div className=" relative  px-4 sm:px-8  flex flex-col justify-center items-center text-center gap-4   font-mulish -translate-y-32 xl:-translate-y-40 ">
+            <h4 className="font-normal text-base xl:text-xl text-secondary">
+              Webdesign Agentur{" "}
+            </h4>
+            <div className="flex flex-col gap-1">
+              <h1 className="text-5xl sm:text-6xl lg:text-7xl  font-extrabold text-white text-center ">
+                Verkaufstarke Websites,{" "}
+              </h1>
+              <h2 className="text-2xl sm:text-4xl lg:text-5xl font-extrabold text-white text-center ">
+                die ihre Kunden überzeugen
+              </h2>
+            </div>
+            <p className=" text-subline font-light   text-sm lg:text-xl">
+              Erzielen Sie nachhaltigen Erfolg und gewinnen Sie loyale Kunden
+              mit einer innovativen Website von RifatIT
+            </p>
+            <button className="px-5 py-2 rounded-md font-bold bg-primary w-fit text-black xl:text-lg shadow-2xl shadow-green-700 ">
+              Jetzt kostenlos beraten lassen
+            </button>
           </div>
-          <p className=" text-subline font-light   text-sm lg:text-xl">
-            Erzielen Sie nachhaltigen Erfolg und gewinnen Sie loyale Kunden mit
-            einer innovativen Website von RifatIT
-          </p>
-          <button className="px-5 py-2 rounded-md font-bold bg-primary w-fit text-black xl:text-lg ">
-            Jetzt kostenlos beraten lassen
-          </button>
-        </div>
-        <div className="mx-auto  flex flex-col justify-center items-center -translate-y-40 mb-10">
-          <IoIosArrowDown
-            className="text-white  text-4xl cursor-pointer"
-            width={50}
-          />
-          <p className="text-subline text-sm">
-            Erfahren Sie mit uns Ihren Umsatz steigern können
-          </p>
+          <div className="  flex flex-col justify-end items-center absolute bottom-10  ">
+            <IoIosArrowDown
+              className="text-white  text-4xl cursor-pointer"
+              width={50}
+            />
+            <p className="text-subline text-sm">
+              Erfahren Sie mit uns Ihren Umsatz steigern können
+            </p>
+          </div>
         </div>
       </div>
 
       <div className="h-screen bg-[#202324] gradientBg2r" id="test">
         <div className="flex flex-col justify-center items-center translate-y-52 gap-5">
           <h4 className="font-normal text-base xl:text-xl text-secondary">
-            Webdesign Agentur{" "}
+            Was wir machen
           </h4>
           <h1 className="text-4xl sm:text-6xl lg:text-7xl  font-extrabold text-white text-center ">
-            Verkaufstarke Websites,{" "}
+            So steigern Sie Ihren Umsatz
           </h1>
         </div>
       </div>
