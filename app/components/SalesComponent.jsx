254b100f2e8ebb391d412e8bc4150f91601ae216
Eremiya Rifat <eremia2021@gmail.com>
2024-04-02 00:16:50 +0200

    animation added

--- app/components/SalesComponent.jsx
+++ app/components/SalesComponent.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const SalesComponent = ({ imageSrc, title, description }) => {
   return (
-    <div className="relative gradient-border flex flex-wrap flex-col py-10 px-6 gap-10 items-center text-center  max-w-[350px]  w-full  min-h-[550px] rounded-xl ">
+    <div className="relative gradient-border flex flex-wrap flex-col py-10 px-6 gap-10 items-center text-center  max-w-[350px]  w-full  min-h-[450px] rounded-xl ">
       <h1 className="font-bold text-3xl md:text-2xl lg:text-2xl">{title}</h1>
       <p className=" font-medium text-sm">{description}</p>
       <div className="absolute top-1/2 w-32 h-32">
--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import SalesComponent from "./components/SalesComponent";
 import GradientBorderComponent from "./components/GradientBorderComponent";
+import { motion } from "framer-motion";
 
 export default function Home() {
   const nextSectionRef = useRef(null);
@@ -36,21 +37,39 @@ export default function Home() {
 
         <div className="  mt-44  text-white px-10 ">
           <div className="flex flex-wrap flex-col mx-auto md:flex-row  max-w-[1440px]  md:gap-10 gap-52 justify-center items-center ">
-            <SalesComponent
-              imageSrc="/radar.svg"
-              title="1. Radar"
-              description="Mit erstklassigem SEO machen wir Ihre Website zum Leuchtturm für Suchende."
-            />
-            <SalesComponent
-              imageSrc="/siegel.svg"
-              title="2. Überzeugung"
-              description="Ihre Professionalität glänzt – Besucher werden sofort von Ihrer Expertise angezogen"
-            />
-            <SalesComponent
-              imageSrc="/euro.svg"
-              title="3. Kunden gewinnen"
-              description="Aus Interessenten werden Kunden, fasziniert von Ihrer Präsenz und dem Wert Ihrer Dienstleistungen."
-            />
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.2, duration: 0.8 }}
+            >
+              <SalesComponent
+                imageSrc="/radar.svg"
+                title="1. Radar"
+                description="Mit erstklassigem SEO machen wir Ihre Website zum Leuchtturm für Suchende."
+              />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.4, duration: 0.8 }}
+            >
+              <SalesComponent
+                imageSrc="/siegel.svg"
+                title="2. Überzeugung"
+                description="Ihre Professionalität glänzt – Besucher werden sofort von Ihrer Expertise angezogen."
+              />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.6, duration: 0.8 }}
+            >
+              <SalesComponent
+                imageSrc="/euro.svg"
+                title="3. Kunden gewinnen"
+                description="Aus Interessenten werden Kunden, fasziniert von Ihrer Präsenz und dem Wert Ihrer Dienstleistungen."
+              />
+            </motion.div>
           </div>
         </div>
       </div>
