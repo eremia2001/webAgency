49b38d25c11906ead74f8a2b46673996628024e6
Eremiya Rifat <eremia2021@gmail.com>
2024-04-11 12:30:30 +0200

    added images to processSteps

--- app/components/ProcessStep.jsx
+++ app/components/ProcessStep.jsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 const ProcessStep = ({ title, description, imageSource }) => {
   return (
-    <div className="flex md:flex-row flex-col justify-between items-center text-white">
+    <div className="flex md:flex-row flex-col justify-between items-center text-white xl:gap-52 gap-32">
       <div className="flex flex-col gap-10 text-left flex-1">
-        <h1 className="text-4xl font-bold text-center md:text-left">{title}</h1>
+        <h1 className="text-3xl font-bold text-center md:text-left">{title}</h1>
         <p className="text-center md:text-left">{description}</p>
       </div>
-      <div className="flex-1">
-        <Image src="/test.svg" width={500} height={500} />
+      <div className="flex-1 max-w-[70%] md:max-w-[30%]">
+        <Image src={imageSource} width={500} height={500} />
       </div>
     </div>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -26,13 +26,13 @@ export default function Home() {
 
       <BenefitsPage />
 
-      <div className="bg-[#202324] pt-56 flex flex-col justify-center items-center ">
+      <div className="bg-[#202324] pt-56 flex flex-col justify-center items-center px-10 ">
         <Title
           title="In 4 Schritten zu Deinem Online Erfolg"
           subtitle="Unser Prozess"
         />
 
-        <div className="flex-1 max-w-[1080px] mt-56 flex flex-col gap-40 px-10">
+        <div className="flex-1 max-w-[1080px] mt-56 flex flex-col xl:gap-56 gap-40 px-10">
           {processSteps.map((step, index) => (
             <ProcessStep
               key={index}
